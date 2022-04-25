90a86e9c4f353c910c251d892acd31fa7b6ba885
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-04-25 13:30:49 +0430

    add button delete fix style

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -4,47 +4,52 @@ import "./SearchBar.css"
 const SearchBar = () => {
     const [value, setValue] = useState('adel')
     const [respons, setRespons] = useState([]);
-    console.log(respons);
     const onFormHandel = (e) => {
         e.preventDefault()
     }
-      const search = async () => {
-          const {data}= await Request.get('api.php', {
-                params: {
-                    srsearch: value
-              }
-          }); 
-            if (data) {
-                setRespons(data.query.search)
-            }     
+    const search = async () => {
+        const { data } = await Request.get('api.php', {
+            params: {
+                srsearch: value
+            }
+        });
+        if (data) {
+            setRespons(data.query.search)
         }
+    };
     useEffect(() => {search()}, [value])
     
     const onInPutHadel = (e) => {
         setValue(e.target.value)
     }
-    const onDelete = (indexs) => {
-         console.log(indexs);
-        // const res = respons.filter((e) => e == indexs)
-        //    console.log(res);
-        // if (res) {
-        //     setRespons(respons.splice(res, 1))
-        // }
-        // console.log(res);
-    
-      
+    const onDeleteItem = (index) => {
+        const newRespons = [...respons]
+        newRespons.splice(index, 1)
+        setRespons(newRespons)
     }
-    const renderlist = respons.map((el,index)=> {
-        return <div style={{ border: '2px solid #777', margin: '10px' }}  key={el.pageid}>
-                <div style={{fontSize:"1.5rem" ,fontWeight:"bold"}}>
-                   {el.title}
-                 </div>
-                   {el.timestamp}
-               <div>
-                 {el.snippet}
-               </div>
-              <i className="close link red icon " onClick={()=>onDelete(index)}></i>
-            </div> })
+    const renderlist = respons.map((el, index) => {
+        return (
+            <div className="items container "
+                key={el.pageid}>
+                <div className="title" >
+                    {el.title}
+                </div >
+                <div className="snippet">
+                      {el.snippet}  
+                </div>
+                <div className="time">
+                    {el.timestamp}
+                </div>
+                
+                <div>
+                   
+                    <i className="close link teal icon circular small inverted " onClick={() => onDeleteItem(index)}></i>
+                </div>
+             
+            </div>
+        )
+    });
+    
     return (
         <>
             <div className="search-bar">
